c34b420fe2cb770d65f3f462426b70843381072f
Mor <mortogo321@gmail.com>
2024-04-22 00:59:19 +0700

    static files

--- server/base/settings.py
+++ server/base/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "static_files"
+STATIC_ROOT = BASE_DIR / "static"
 
 CACHES = {
     "default": {
--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -3,4 +3,7 @@
 echo 'Running migrations...'
 python manage.py migrate
 
+echo 'Collecting static files...'
+python manage.py collectstatic --no-input
+
 exec "$@"
