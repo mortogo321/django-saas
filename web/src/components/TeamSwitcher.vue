be4aac0328bd0d7902dbefc0d7ba3844b9faaeed
Mor <mortogo321@gmail.com>
2024-05-09 09:02:38 +0700

    fix command filters

--- web/src/components/TeamSwitcher.vue
+++ web/src/components/TeamSwitcher.vue
@@ -1,188 +1,62 @@
 <script setup lang="ts">
-import { CaretSortIcon, CheckIcon, PlusCircledIcon } from '@radix-icons/vue'
-import { ref } from 'vue'
+import {
+    Calculator,
+    Calendar,
+    CreditCard,
+    Settings,
+    Smile,
+    User,
+} from 'lucide-vue-next';
 
 import {
-    Avatar,
-    AvatarFallback,
-    AvatarImage,
-} from '@/components/ui/avatar'
-import { Button } from '@/components/ui/button'
-import { cn } from '@/lib/utils'
-
-import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList, CommandSeparator } from '@/components/ui/command'
-import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-} from '@/components/ui/dialog'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
-import {
-    Popover,
-    PopoverContent,
-    PopoverTrigger,
-} from '@/components/ui/popover'
-import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-} from '@/components/ui/select'
-
-const groups = [
-    {
-        label: 'Personal Account',
-        teams: [
-            {
-                label: 'Alicia Koch',
-                value: 'personal',
-            },
-        ],
-    },
-    {
-        label: 'Teams',
-        teams: [
-            {
-                label: 'Acme Inc.',
-                value: 'acme-inc',
-            },
-            {
-                label: 'Monsters Inc.',
-                value: 'monsters',
-            },
-        ],
-    },
-]
-
-type Team = (typeof groups)[number]['teams'][number]
-
-const open = ref(false)
-const showNewTeamDialog = ref(false)
-const selectedTeam = ref<Team>(groups[0].teams[0])
+    Command,
+    CommandEmpty,
+    CommandGroup,
+    CommandInput,
+    CommandItem,
+    CommandList,
+    CommandSeparator,
+    CommandShortcut,
+} from '@/components/ui/command';
 </script>
 
 <template>
-    <Dialog v-model:open="showNewTeamDialog">
-        <Popover v-model:open="open">
-            <PopoverTrigger as-child>
-                <Button
-                    variant="outline"
-                    role="combobox"
-                    aria-expanded="open"
-                    aria-label="Select a team"
-                    :class="cn('w-[200px] justify-between', $attrs.class ?? '')">
-                    <Avatar class="mr-2 h-5 w-5">
-                        <AvatarImage
-                            :src="`https://avatar.vercel.sh/${selectedTeam.value}.png`"
-                            :alt="selectedTeam.label" />
-                        <AvatarFallback>SC</AvatarFallback>
-                    </Avatar>
-                    {{ selectedTeam.label }}
-                    <CaretSortIcon class="ml-auto h-4 w-4 shrink-0 opacity-50" />
-                </Button>
-            </PopoverTrigger>
-            <PopoverContent class="w-[200px] p-0">
-                <Command :filter-function="(list, term) => list.filter(i => i.label?.toLowerCase()?.includes(term))">
-                    <CommandList>
-                        <CommandInput placeholder="Search team..." />
-                        <CommandEmpty>No team found.</CommandEmpty>
-                        <CommandGroup v-for="group in groups" :key="group.label" :heading="group.label">
-                            <CommandItem
-                                v-for="team in group.teams"
-                                :key="team.value"
-                                :value="team"
-                                class="text-sm"
-                                @select="() => {
-                                    selectedTeam = team
-                                    open = false
-                                }">
-                                <Avatar class="mr-2 h-5 w-5">
-                                    <AvatarImage
-                                        :src="`https://avatar.vercel.sh/${team.value}.png`"
-                                        :alt="team.label"
-                                        class="grayscale" />
-                                    <AvatarFallback>SC</AvatarFallback>
-                                </Avatar>
-                                {{ team.label }}
-                                <CheckIcon
-                                    :class="cn('ml-auto h-4 w-4',
-                                        selectedTeam.value === team.value
-                                            ? 'opacity-100'
-                                            : 'opacity-0',
-                                    )" />
-                            </CommandItem>
-                        </CommandGroup>
-                    </CommandList>
-                    <CommandSeparator />
-                    <CommandList>
-                        <CommandGroup>
-                            <DialogTrigger as-child>
-                                <CommandItem
-                                    value="create-team"
-                                    @select="() => {
-                                        open = false
-                                        showNewTeamDialog = true
-                                    }">
-                                    <PlusCircledIcon class="mr-2 h-5 w-5" />
-                                    Create Team
-                                </CommandItem>
-                            </DialogTrigger>
-                        </CommandGroup>
-                    </CommandList>
-                </Command>
-            </PopoverContent>
-        </Popover>
-        <DialogContent>
-            <DialogHeader>
-                <DialogTitle>Create team</DialogTitle>
-                <DialogDescription>
-                    Add a new team to manage products and customers.
-                </DialogDescription>
-            </DialogHeader>
-            <div>
-                <div class="space-y-4 py-2 pb-4">
-                    <div class="space-y-2">
-                        <Label for="name">Team name</Label>
-                        <Input id="name" placeholder="Acme Inc." />
-                    </div>
-                    <div class="space-y-2">
-                        <Label for="plan">Subscription plan</Label>
-                        <Select>
-                            <SelectTrigger>
-                                <SelectValue placeholder="Select a plan" />
-                            </SelectTrigger>
-                            <SelectContent>
-                                <SelectItem value="free">
-                                    <span class="font-medium">Free</span> -
-                                    <span class="text-muted-foreground">
-                                        Trial for two weeks
-                                    </span>
-                                </SelectItem>
-                                <SelectItem value="pro">
-                                    <span class="font-medium">Pro</span> -
-                                    <span class="text-muted-foreground">
-                                        $9/month per user
-                                    </span>
-                                </SelectItem>
-                            </SelectContent>
-                        </Select>
-                    </div>
-                </div>
-            </div>
-            <DialogFooter>
-                <Button variant="outline" @click="showNewTeamDialog = false">
-                    Cancel
-                </Button>
-                <Button type="submit">
-                    Continue
-                </Button>
-            </DialogFooter>
-        </DialogContent>
-    </Dialog>
+    <Command class="rounded-lg border shadow-md max-w-[450px]">
+        <CommandInput placeholder="Type a command or search..." />
+        <CommandList>
+            <CommandEmpty>No results found.</CommandEmpty>
+            <CommandGroup heading="Suggestions">
+                <CommandItem value="Calendar">
+                    <Calendar class="mr-2 h-4 w-4" />
+                    <span>Calendar</span>
+                </CommandItem>
+                <CommandItem value="Search Emoji">
+                    <Smile class="mr-2 h-4 w-4" />
+                    <span>Search Emoji</span>
+                </CommandItem>
+                <CommandItem value="Calculator">
+                    <Calculator class="mr-2 h-4 w-4" />
+                    <span>Calculator</span>
+                </CommandItem>
+            </CommandGroup>
+            <CommandSeparator />
+            <CommandGroup heading="Settings">
+                <CommandItem value="Profile">
+                    <User class="mr-2 h-4 w-4" />
+                    <span>Profile</span>
+                    <CommandShortcut>⌘P</CommandShortcut>
+                </CommandItem>
+                <CommandItem value="Billing">
+                    <CreditCard class="mr-2 h-4 w-4" />
+                    <span>Billing</span>
+                    <CommandShortcut>⌘B</CommandShortcut>
+                </CommandItem>
+                <CommandItem value="Settings">
+                    <Settings class="mr-2 h-4 w-4" />
+                    <span>Settings</span>
+                    <CommandShortcut>⌘S</CommandShortcut>
+                </CommandItem>
+            </CommandGroup>
+        </CommandList>
+    </Command>
 </template>
