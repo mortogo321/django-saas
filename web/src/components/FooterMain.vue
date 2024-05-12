8caa24d1d8e249e3b3beaabcf0c20df5452a65c7
Mor <mortogo321@gmail.com>
2024-05-12 17:31:42 +0700

    add bootstrap css

---
+++ web/src/components/FooterMain.vue
@@ -0,0 +1,7 @@
+<template>
+    <footer class="footer mt-auto py-3 bg-body-tertiary">
+        <div class="container">
+            <span class="text-body-secondary">&copy;KaliDocs</span>
+        </div>
+    </footer>
+</template>
--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+    <h1>Home page</h1>
+  </div>
 </template>
